7d305ed370170b9692d28c4dbd472a00fd43b3fa
Nam Tran <tranngocnam97@yahoo.com>
2016-10-30 11:05:19 +0000

    Fixed some things

--- tests/firefighter_tests.py
+++ tests/firefighter_tests.py
@@ -1,11 +1,15 @@
 from nose.tools import *
-import NAME
+from firefighter import firefighter
 
-def setup():
-    print "SETUP!"
+# def setup():
+#     print "SETUP!"
 
-def teardown():
-    print "TEAR DOWN!"
+# def teardown():
+#     print "TEAR DOWN!"
 
-def test_basic():
-    print "I RAN!"
+# def test_basic():
+#     print "I RAN!"
+
+def test_config():
+    assert_equal(firefighter.get_pin('input', 'input_example1'), '1')
+    assert_equal(firefighter.get_pin('output', 'output_example1'), '2')
