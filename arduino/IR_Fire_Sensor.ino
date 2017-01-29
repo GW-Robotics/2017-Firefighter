b549097b34422b05a4b3407d589874bc943203e6
Bwall72 <Brandonw@live.com>
2017-01-29 11:46:45 -0500

    Add files via upload

---
+++ arduino/IR_Fire_Sensor.ino
@@ -0,0 +1,21 @@
+//Brandon Waller
+//GW Robotics
+//Super Complicated code for IR Fire Sensor
+
+const int SensorPin = 2;
+void setup() {
+  Serial.begin(9600);
+  pinMode(SensorPin, INPUT);
+}
+
+void loop() {
+  if(digitalRead(SensorPin) == HIGH)
+  {
+    Serial.println("There is a Fire");
+  }
+  else
+  {
+    Serial.println("There is no Fire");
+  }
+  delay(2000);
+}
