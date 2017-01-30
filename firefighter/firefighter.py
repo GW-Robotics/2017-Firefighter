8bc6dbe22adfb454642f3878e7f49274fae56cf2
omn0mn0m <tranngocnam97@yahoo.com>
2017-01-30 00:04:53 -0500

    Bug fixes

--- firefighter/firefighter.py
+++ firefighter/firefighter.py
@@ -1,19 +1,21 @@
 import robotmap
 
 from nanpy import ArduinoApi, SerialManager
+from sensors import FlameSensor
 
-class FlameSensor(object):
-
-    def __init__(self, pin):
-        self.sensor_pin = pin
-        print 'Flame Sensor Connected To: ' + self.sensor_pin
-
-        Arduino.pinMode(self.sensor_pin, Arduino.INPUT)
-
-    def is_flame_detected(self):
-        return Arduino.digitalRead(self.sensor_pin) == Arduino.HIGH
+#class FlameSensor(object):
+#
+#    def __init__(self, pin):
+#        self.sensor_pin = pin
+#        print 'Flame Sensor Connected To: ' + self.sensor_pin
+#
+#        Arduino.pinMode(self.sensor_pin, Arduino.INPUT)
+#
+#    def is_flame_detected(self):
+#        return Arduino.digitalRead(self.sensor_pin) == Arduino.HIGH
 
 
 flamesensor = FlameSensor(robotmap.get_pin('input', 'flamesensor'))
 
-print flamesensor.is_flame_detected()
+while (True):
+    print flamesensor.is_flame_detected()
