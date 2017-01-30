83e2cdbf0d7c400269e18afacb774ee4b13d2c08
omn0mn0m <tranngocnam97@yahoo.com>
2017-01-29 22:42:07 -0500

    Tested flame sensor and created a wiring map module

--- firefighter/firefighter.py
+++ firefighter/firefighter.py
@@ -1,11 +1,9 @@
-import os
-from ConfigParser import ConfigParser
-from nanpy import ArduinoApi, SerialManager
+import robotmap
 
-assert os.path.exists('firefighter/wiring.cfg')
+from nanpy import ArduinoApi, SerialManager
+from sensors import FlameSensor
 
 connected_to_arduino = False
-VERBOSE = True
 
 try:
     connection = SerialManager(device = '/dev/ttyACM0')
@@ -13,42 +11,20 @@ try:
     connected_to_arduino = True
 except:
     connected_to_arduino = False
-
-
-# Motor class that will be used to control individual motors on the robot
-class Motor(object):
     
+
+class FlameSensor(object):
+
     def __init__(self, pin):
-        self.pin = pin
-        if connected_to_arduino:
-            Arduino.pinMode(self.pin, Arduino.OUTPUT)
-        
-    def set_speed(self, speed):
-        if VERBOSE:
-            print "Setting speed to " + str(speed)
-            
-        if connected_to_arduino:
-            Arduino.digitalWrite(speed)
+        self.sensor_pin = pin
+        print 'Flame Sensor Connected To: ' + self.sensor_pin
+
+        Arduino.pinMode(self.sensor_pin, Arduino.INPUT)
+
+    def is_flame_detected(self):
+        return Arduino.digitalRead(self.sensor_pin) == Arduino.HIGH
 
 
-# Reads the config file into the program
-config = ConfigParser()
-config.read('firefighter/wiring.cfg')
+flamesensor = FlameSensor(robotmap.get_pin('input', 'flamesensor'))
 
-input_pins = config.items("Input")
-output_pins = config.items("Output")
-
-# Gets the value of a pin based on the name of a value in the wiring.cfg file
-def get_pin(pin_type, name):
-    if pin_type == 'input':
-        for pair in input_pins:
-            if pair[0] == name:
-                return pair[1]
-    elif pin_type == 'output':
-        for pair in output_pins:
-            if pair[0] == name:
-                return pair[1]
-
-# # Example of making a motor and setting its speed to 100%
-# input_example1 = Motor(get_pin('input', 'input_example1'))
-# input_example1.set_speed(1.0)
+print flamesensor.is_flame_detected()
