2c33b7e25046d6f82038f9aa2da0253cef2cf011
Nam Tran <tranngocnam97@yahoo.com>
2017-02-06 15:24:27 -0500

    Added blank functions for the required LED functions

--- firefighter/firefighter.py
+++ firefighter/firefighter.py
@@ -15,9 +15,27 @@ Arduino.pinMode(13, Arduino.OUTPUT)
 
 flamesensor = FlameSensor(robotmap.get_pin('input', 'flamesensor'))
 
+raw_input("Press ENTER to enable robot")
+
 while (True):
     print flamesensor.is_flame_detected()
     Arduino.digitalWrite(13, Arduino.HIGH)
     sleep(0.3)
     Arduino.digitalWrite(13, Arduino.LOW)
     sleep(0.3)
+
+def detected_sound(self):
+    # Runs when a sound is detected
+    pass
+
+def detected_flame(self):
+    # Runs when a flame is detected
+    pass
+
+def detected_video(self):
+    # Runs when a computer vision is being used:
+    # Solid: baby drop-off target detected
+    # Blinking: cradle target detected
+    pass
+
+
