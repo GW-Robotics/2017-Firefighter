f15611ee777df3bb98cbc45fa9dfecdc840bb3b7
omn0mn0m <tranngocnam97@yahoo.com>
2017-02-17 20:05:25 -0500

    Began working on blob detection

--- firefighter/visionprocessor.py
+++ firefighter/visionprocessor.py
@@ -5,7 +5,11 @@ import numpy as np
 #frame = cv2.imread('../resources/blue-0-0-255.png')
 capture = cv2.VideoCapture(0)
 
-def simple_mask():
+blob_detector = cv2.SimpleBlobDetector_create()
+
+    
+while(1):
+    # For demonstration
     # captures each frame
     _, frame = capture.read()
     
@@ -17,19 +21,17 @@ def simple_mask():
     
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
     res = cv2.bitwise_and(frame, frame, mask = mask)
+
+    keypoints = blob_detector.detect(res)
+
+    im_with_keypoints = cv2.drawKeypoints(res, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     
     # for debugging only
     cv2.imshow('frame', frame)
     cv2.imshow('mask', mask)
     cv2.imshow('res', res)
+    cv2.imshow('blobs', im_with_keypoints)
     
-
-def image_threshold():
-    pass
-    
-while(1):
-    # For demonstration
-    simple_mask()
 
     # TODO: Determine primary colour in image
     # TODO: Apply image thresholding method
@@ -39,5 +41,6 @@ while(1):
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
-    
+
+capture.release()
 cv2.destroyAllWindows()
