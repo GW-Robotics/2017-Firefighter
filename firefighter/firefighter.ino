9b8c58957c42eddc9722191967577513aa8cc224
Nam Tran <tranngocnam97@yahoo.com>
2017-03-30 01:45:06 -0400

    Began work on mpu6050

--- firefighter/firefighter.ino
+++ firefighter/firefighter.ino
@@ -1,10 +1,16 @@
 #include "Motor.h"
+#include "Ultrasonic.h"
 
-Motor leftMotor1(2, 3);
+Motor leftMotor1(12, 13);
 Motor leftMotor2(4, 5);
 Motor rightMotor1(6, 7);
 Motor rightMotor2(8, 9);
 Motor strafeMotor(10, 11);
+
+Ultrasonic frontUltrasonic(22, 23, true);
+Ultrasonic leftUltrasonic(24, 25, true);
+Ultrasonic rightUltrasonic(26, 27, true);
+Ultrasonic backUltrasonic(28, 29, true);
 
 void hDrive(double move, double rotate, double strafe) {
   double leftSpeed, rightSpeed;
@@ -40,9 +46,13 @@ void hDrive(double move, double rotate, double strafe) {
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  hDrive(1.0, 0.0, 0.0);
+  //hDrive(1.0, 0.0, 0.0);
+  Serial.println(1.0);
+  delay(400);
 }
+
