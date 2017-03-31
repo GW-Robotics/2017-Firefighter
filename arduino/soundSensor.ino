df3c41274b510c50b544e4ab90902076bb6b5106
jwalrusisawesome <jeremeywaldron15@gmail.com>
2017-03-31 14:19:23 -0400

    Made sound sensor work
    used for start

---
+++ arduino/soundSensor.ino
@@ -0,0 +1,34 @@
+
+int sensorPin = A0;    
+int ledPin = 13;      
+int sensorValue = 0;
+bool robotOn = false;
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  Serial.begin(112500);
+}
+
+void soundStart(){
+  
+  sensorValue = analogRead(sensorPin);
+  Serial.println("Sensor Value: " + (String)sensorValue);
+  Serial.print( "  RobotOn: " +  (String)robotOn) ;
+  delay(100);
+  
+    if(sensorValue > 500) {
+      robotOn = true;
+    }
+    while(robotOn){
+      for(int i = 0; i <100; i++){
+       digitalWrite(ledPin, HIGH);
+       Serial.println( "RobotOn: TRUE") ;
+      }
+    }    
+
+}
+void loop() {
+ soundStart();
+}
