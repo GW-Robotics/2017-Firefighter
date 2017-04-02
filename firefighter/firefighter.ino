4e7c1241ad50bb47de5fd09fca2b60887f526de1
Nam Tran <tranngocnam97@yahoo.com>
2017-04-01 22:54:28 -0400

    Added gyro

--- firefighter/firefighter.ino
+++ firefighter/firefighter.ino
@@ -4,8 +4,16 @@
 #include "FreqCount.h"
 #include <Servo.h>
 
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+//#include "MPU6050.h" // not necessary if using MotionApps include file
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+#define SOUND_LED 43
 #define FLAME_LED 53
-#define SOUND_LED 42
 
 #define WALL_DISTANCE 7
 
@@ -16,9 +24,14 @@
 #define LOW_START 3230
 #define HIGH_START 4370
 
+#define LEFT_MOTOR_1A 42
+#define LEFT_MOTOR_1B 3
+
+#define INTERRUPT_PIN 2
+
 Servo extingusher_servo;
 
-Motor leftMotor1(2, 3);
+Motor leftMotor1(LEFT_MOTOR_1A, LEFT_MOTOR_1B);
 Motor leftMotor2(4, 5);
 Motor rightMotor1(6, 7);
 Motor rightMotor2(8, 9);
@@ -32,6 +45,8 @@ Ultrasonic leftUltrasonic(26, 27, true);
 Ultrasonic rightUltrasonic(24, 25, true);
 Ultrasonic backUltrasonic(22, 23, true);
 
+MPU6050 mpu; // Default address 0x68
+
 // Drivetrain commands
 void driveToWhite();
 void driveThroughRoom();
@@ -39,6 +54,7 @@ void hDrive(double move, double rotate, double strafe);
 void tankDrive(double leftSpeed, double rightSpeed, double strafeSpeed);
 void definedStartSearch();
 double getAngle(double targetAngle);
+double getAngle(char axis);
 double getPercentError(double actual, double target);
 void turnToAngle(double targetAngle, double speed);
 void naviguessMaze(double swagSpeed);
@@ -72,9 +88,34 @@ void extinguish();
 
 int startSwitch = 14;
 
+// Gyro
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+float euler[3];         // [psi, theta, phi]    Euler angle container
+
 int ri;
 
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
 void setup() {
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+      Wire.begin();
+      Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+      Fastwire::setup(400, true);
+  #endif
+    
   Serial.begin(9600);
   
   // Start Switch
@@ -102,6 +143,34 @@ void setup() {
   extingusher_servo.attach(servo_pin);
   extingusher_servo.write(90);
 
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+  
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+      // turn on the DMP, now that it's ready
+      mpu.setDMPEnabled(true);
+
+      // enable Arduino interrupt detection
+      attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+      mpuIntStatus = mpu.getIntStatus();
+
+      // set our DMP Ready flag so the main loop() function knows it's okay to use it
+      Serial.println(F("DMP ready! Waiting for first interrupt..."));
+      dmpReady = true;
+
+      // get expected DMP packet size for later comparison
+      packetSize = mpu.dmpGetFIFOPacketSize();
+  }
+
   FreqCount.begin(1000);
 }
 
@@ -124,23 +193,15 @@ void loop() {
 
   if(robotOn){
     FreqCount.end();
-	// Serial.println(colour_sensor.getColor('r'));
     // ultrasonicCheck();
 	// drivetrainCheck();
 	if (ri == -1) {
 		ri = colour_sensor.getColor('r');
 	}
 	
-	// definedStartSearch();
-	
- 
-	// while((colour_sensor.getColor('r') > ri - 70)) {
-		// naviguessMaze(0.3);
-	// }
-	
 	while(!foundFlame && digitalRead(flame_sensor)) {
 		// naviguessMaze(0.3);
-		followWall(3);
+		followWall(1.0);
 	}
 	
 	foundFlame = true;
@@ -163,20 +224,6 @@ void loop() {
 	}
 
 	hDrive(0.0, 0.0, 0.0);
-	
-	// while (true) {
-		// for(int i = 40; i <= 160; i += interval){
-		  // extingusher_servo.write(i); 
-		// }
-	  
-		// for(int i = 140; i >=20; i -= interval){
-		  // extingusher_servo.write(i); 
-		// }
-	// }
-
-	// while (!foundRoom) {
-		// driveToWhite();
-	// }
   }
   else{
     digitalWrite(SOUND_LED, LOW);
@@ -409,28 +456,7 @@ void naviguessMaze(double swagSpeed) {
 	bool rightTriggered = rightUltrasonic.getDistance() < 3;
 	bool backTriggered = backUltrasonic.getDistance() < 2;
 	
-	// bool sweetSpot =  false;
-	
-	// if (frontUltrasonic.getDistance() < 2) {
-		 // frontTriggered = true;
-	// } 
-	// // else if (leftUltrasonic.getDistance() > 3) && (rightUltrasonic.getDistance() > 4)) {
-		// // sweetSpot = true;
-	// } else if (leftUltrasonic.getDistance() < 3) {
-		// leftTriggered = true;
-		// frontTriggered = frontUltrasonic.getDistance() < 2;
-	// } else if (rightUltrasonic.getDistance() < 3) {
-		// rightTriggered = true;
-		// frontTriggered = frontUltrasonic.getDistance() < 2;
-	// } else if (backUltrasonic.getDistance() < 2) {
-		// backTriggered = true;
-	// } else {
-		// frontTriggered = leftTriggered = rightTriggered = backTriggered = sweetSpot = false;
-	// }
-	
-	// if (sweetSpot) {
-		// hDrive(swagSpeed, 0.0, 0.0);
-	  if (leftTriggered && frontTriggered) {
+	if (leftTriggered && frontTriggered) {
 		hDrive(-swagSpeed, 0.0, 0.0);
 		delay(100);
 		// hDrive(0.0, swagSpeed , 0.0);
@@ -499,10 +525,72 @@ void tankDrive(double leftSpeed, double rightSpeed, double strafeSpeed) {
 
 void followWall(double targetDistance) {
 	if (rightUltrasonic.getDistance() < targetDistance) {
-		tankDrive(0.6, 0.3, 0.0);
+		tankDrive(0.0, 0.3, 0.0);
 	} else if (rightUltrasonic.getDistance() > targetDistance) {
-		tankDrive(0.3, 0.6, 0.0);
+		tankDrive(0.3, 0.0, 0.0);
 	} else {
-		tankDrive(0.6, 0.6, 0.0);
+		tankDrive(0.3, 0.4, 0.0);
 	}
 }
+
+double getAngle(char axis) {
+  // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        // other program behavior stuff here
+        // .
+        // .
+        // .
+        // if you are really paranoid you can frequently test in between other
+        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+        // while() loop to immediately process the MPU data
+        // .
+        // .
+        // .
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+        
+        // display Euler angles in degrees
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetEuler(euler, &q);
+
+        switch (axis) {
+          case 'x':
+            return euler[2] * 180/M_PI;
+            break;
+          case 'y':
+            return euler[1] * 180/M_PI;
+            break;
+          case 'z':
+            return euler[0] * 180/M_PI;
+            break;
+        }
+    }
+}
+
