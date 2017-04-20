b85b3a740075f4713b804bbd5e1a0dbead982372
omn0mn0m <tranngocnam97@yahoo.com>
2017-04-20 01:13:28 -0400

    Working RPi freq start

--- Python-Project/firefighter/firefighter.py
+++ Python-Project/firefighter/firefighter.py
@@ -29,14 +29,16 @@ def drive_in_square():
     drivetrain.arcade_drive(0.0, 0.0, -1.0)
     sleep(1)
 
-while (not status_light.activation_switch_pressed()):
+while (not (status_light.is_freq_start() or status_light.activation_switch_pressed())):
     pass
 
 sleep(2)
+print "Started!"
 
-#while (not status_light.activation_switch_pressed()):
+while (not status_light.activation_switch_pressed()):
     #sensor_stick.check_actuation()
-drivetrain.drive_to_front()
+    #drivetrain.drive_to_front()
     #drive_in_square()
+    pass
     
 drivetrain.stop()
